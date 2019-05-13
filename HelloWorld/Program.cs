6b492db20808180f4a04b9f537f5a578e8dfc69f
Mark Palmer <mark.palmer@itsalllive.com>
2019-05-13 15:56:04 -0400

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my first program");
+            Console.WriteLine("and it works!");
+            
+            Console.ReadKey();
+        }
+    }
+}
